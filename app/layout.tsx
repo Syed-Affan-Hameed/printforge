98442c7596b1eba24f735ac622c506e3bd9cc972
Syed Affan Hameed <syedaffanhameed@gmail.com>
2025-10-26 23:54:14 +0530

    Implement code structure updates and remove redundant sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
+import Image from "next/image"
+import heroImage from "@/public/aboutscreen_image.png"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const albertSans = Albert_Sans({
   subsets: ["latin"],
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const montserratAlternates = Montserrat_Alternates({
   subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat-alternates",
 });
 
 export const metadata: Metadata = {
@@ -25,9 +29,45 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${albertSans.className} ${montserratAlternates.variable} antialiased`}
       >
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop logo */}
+              <img
+                src="/printforge-logo.svg"
+                alt="PrintForge Logo"
+                className="w-[200px] h-auto hidden md:block"
+              />
+                <Image
+                            src={heroImage}
+                            alt="PrintForge Community - A group of makers collaborating on 3D printing projects"
+                            width={742}
+                            height={742}
+                              className="w-[200px] h-auto hidden md:block"       />
+              {/* Mobile logo */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="PrintForge Logo"
+                className="w-[40px] h-auto block md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5">
+              <p>3D Models</p>
+              <p>About</p>
+            </ul>
+          </nav>
+        </header>
         {children}
+
+        <footer>
+          <ul>
+            <li>Email</li>
+            <li>Social Media</li>
+            <li>Trademark</li>
+          </ul>
+        </footer>
       </body>
     </html>
   );
